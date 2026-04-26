f359d52137a9af51bd9b92d894856c58252a81fa
rkhanra <rohitkhanra420@gmail.com>
2026-04-26 20:22:42 +0530

    3rd update

--- client/script.js
+++ client/script.js
@@ -5,6 +5,7 @@ const token = localStorage.getItem("token");
 if (!token) {
   window.location.href = "login.html";
 }
+// const token = localStorage.getItem("token");
 
 
 // ==============================
